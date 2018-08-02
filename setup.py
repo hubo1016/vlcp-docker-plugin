571ee55578572128568347fa28223ece5a3f1fb9
hubo <hubo1016@126.com>
2018-08-02 13:53:10 +0800

    migrating to VLCP 2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
     pass
 from setuptools import setup, find_packages
 
-VERSION = '0.2.7'
+VERSION = '0.3.0'
 
 setup(name='vlcp-docker-plugin',
       version=VERSION,
@@ -23,5 +23,7 @@ setup(name='vlcp-docker-plugin',
       keywords=['SDN', 'VLCP', 'docker'],
       test_suite = 'tests',
       use_2to3=False,
-      install_requires = ["vlcp>=1.2.3"],
-      packages=find_packages(exclude=("tests","tests.*","misc","misc.*")))
+      install_requires = ["vlcp>=2.0.0"],
+      packages=find_packages(exclude=("tests","tests.*","misc","misc.*")),
+      python_requires='>=3.5'
+)
